fb5d743371bdf4565e3d1487a6839f576a30cc4a
Ibrahim Abdulmalik <ibrahimmalik095@gmail.com>
2023-05-25 13:29:52 +0100

    change app and display name

--- android/app/src/main/java/com/opencart/newarchitecture/components/MainComponentsRegistry.java
+++ android/app/src/main/java/com/opencart/newarchitecture/components/MainComponentsRegistry.java
@@ -1,4 +1,4 @@
-package com.opencart.newarchitecture.components;
+package com.RNTodo.newarchitecture.components;
 
 import com.facebook.jni.HybridData;
 import com.facebook.proguard.annotations.DoNotStrip;
--- android/app/src/main/java/com/opencart/newarchitecture/modules/MainApplicationTurboModuleManagerDelegate.java
+++ android/app/src/main/java/com/opencart/newarchitecture/modules/MainApplicationTurboModuleManagerDelegate.java
@@ -1,4 +1,4 @@
-package com.opencart.newarchitecture.modules;
+package com.RNTodo.newarchitecture.modules;
 
 import com.facebook.jni.HybridData;
 import com.facebook.react.ReactPackage;
