aa5ae27e21c850807ea5eb752b76de4af97a1a4e
Ibrahim Abdulmalik <ibrahimmalik095@gmail.com>
2023-05-29 15:10:41 +0100

    feat: complete get task function

--- src/screens/TodoScreen/index.js
+++ src/screens/TodoScreen/index.js
@@ -1,20 +1,60 @@
-import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
-import React from 'react';
+import {FlatList, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import React, {useEffect} from 'react';
 import Container from '../../components/Container';
 import FontAwesome5Icon from 'react-native-vector-icons/FontAwesome5';
+import {useDispatch, useSelector} from 'react-redux';
+import {setTasks, setTaskID} from '../../store/taskStore/taskActions';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import CheckBox from '@react-native-community/checkbox';
+import GlobalStyle from '../../utils/GlobalStyle';
 
 const TodoScreen = ({navigation}) => {
+  const {tasks} = useSelector(state => state.taskReducer);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    getTasks();
+  }, []);
+
+  const getTasks = () => {
+    AsyncStorage.getItem('Tasks')
+      .then(tasks => {
+        const parsedTasks = JSON.parse(tasks);
+        if (parsedTasks && typeof parsedTasks === 'object') {
+          dispatch(setTasks(parsedTasks));
+        }
+      })
+      .catch(err => console.log(err));
+  };
   return (
-    <Container>
-      <View style={styles.container}>
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => {
-            navigation.navigate('Task');
-          }}>
-          <FontAwesome5Icon name={'plus'} size={20} color={'#ffffff'} />
-        </TouchableOpacity>
-      </View>
+    <Container noTopInset>
+      {/* <View style={styles.container}> */}
+      <FlatList
+        data={tasks}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({item}) => {
+          return (
+            <TouchableOpacity
+              style={styles.item}
+              onPress={() => {
+                dispatch(setTaskID(item.ID));
+                navigation.navigate('TaskScreen');
+              }}>
+              <Text style={styles.title}>{item.Title}</Text>
+              <Text style={styles.subtitle}>{item.Desc}</Text>
+            </TouchableOpacity>
+          );
+        }}
+      />
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => {
+          dispatch(setTaskID(tasks.length + 1));
+          navigation.navigate('TaskScreen');
+        }}>
+        <FontAwesome5Icon name={'plus'} size={20} color={'#ffffff'} />
+      </TouchableOpacity>
+      {/* </View> */}
     </Container>
   );
 };
@@ -36,5 +76,24 @@ const styles = StyleSheet.create({
     bottom: 10,
     right: 10,
     elevation: 5,
+  },
+  item: {
+    marginHorizontal: 10,
+    marginVertical: 7,
+    paddingRight: 10,
+    backgroundColor: '#ffffff',
+    justifyContent: 'center',
+    borderRadius: 10,
+    elevation: 5,
+  },
+  title: {
+    color: '#000000',
+    fontSize: 30,
+    margin: 5,
+  },
+  subtitle: {
+    color: '#999999',
+    fontSize: 20,
+    margin: 5,
   },
 });
