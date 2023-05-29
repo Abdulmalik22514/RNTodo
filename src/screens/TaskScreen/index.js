d03e429b8b7109adf6dbd404331e94c5a1d2672a
Ibrahim Abdulmalik <ibrahimmalik095@gmail.com>
2023-05-29 15:10:27 +0100

    feat: complete add task function

--- src/screens/TaskScreen/index.js
+++ src/screens/TaskScreen/index.js
@@ -1,13 +1,63 @@
-import {StyleSheet, Text, TextInput, View} from 'react-native';
-import React, {useState} from 'react';
+import {Alert, StyleSheet, Text, TextInput, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
 import Container from '../../components/Container';
 import CustomButton from '../../components/CustomButton';
+import {useDispatch, useSelector} from 'react-redux';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {setTasks} from '../../store/taskStore/taskActions';
 
-const TaskScreen = () => {
+const TaskScreen = ({navigation}) => {
   const [title, setTitle] = useState('');
   const [desc, setDesc] = useState('');
+
+  const {tasks, taskID} = useSelector(state => state.taskReducer);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    getTask();
+  }, []);
+
+  const getTask = () => {
+    const Task = tasks.find(task => task.ID === taskID);
+    if (Task) {
+      setTitle(Task.Title);
+      setDesc(Task.Desc);
+    }
+  };
+
+  const setTask = () => {
+    if (title.length == 0) {
+      Alert.alert('Warning!', 'Please write your task title.');
+    } else {
+      try {
+        let Task = {
+          ID: taskID,
+          Title: title,
+          Desc: desc,
+        };
+        const index = tasks.findIndex(task => task.ID === taskID);
+        let newTasks = [];
+        if (index > -1) {
+          newTasks = [...tasks];
+          newTasks[index] = Task;
+        } else {
+          newTasks = [...tasks, Task];
+        }
+        AsyncStorage.setItem('Tasks', JSON.stringify(newTasks))
+          .then(() => {
+            dispatch(setTasks(newTasks));
+            Alert.alert('Success!', 'Task saved successfully.');
+            navigation.goBack();
+          })
+          .catch(err => console.log(err));
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
   return (
-    <Container>
+    <Container noTopInset>
       <View style={styles.container}>
         <TextInput
           value={title}
@@ -26,7 +76,7 @@ const TaskScreen = () => {
           title="Save Task"
           color="#1eb900"
           style={{width: '100%'}}
-          // onPressFunction={setTask}
+          onPressFunction={setTask}
         />
       </View>
     </Container>
@@ -50,5 +100,6 @@ const styles = StyleSheet.create({
     fontSize: 20,
     margin: 10,
     paddingHorizontal: 10,
+    height: 50,
   },
 });
